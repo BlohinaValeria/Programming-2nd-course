5855ab7520fe761f2c27b28c2d8c1f178836288a
Blohina Valeria <159047201+BlohinaValeria@users.noreply.github.com>
2024-11-24 20:52:11 +0300

    Add files via upload

--- Лабораторная 4/LAB4_2_3.py
+++ Лабораторная 4/LAB4_2_3.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Создание окна с двумя графиками
-x1 = np.linspace(-2, 6, 100)
-y1 = x1**2 - 4*x1 + 4  # f(x) = x^2 - 4x + 4
-y2 = 2*x1 + 3          # f(x) = 2x + 3
 
+x1 = np.linspace(-2, 6, 100)
+y1 = x1 ** 2 - 4 * x1 + 4  # f(x) = x^2 - 4x + 4
+y2 = 2 * x1 + 3  # f(x) = 2x + 3
 plt.figure(1)
 plt.subplot(2, 1, 1)
 plt.plot(x1, y1, label='f(x) = x^2 - 4x + 4', color='green')
@@ -14,26 +14,28 @@ plt.grid(True)
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.legend()
-
-plt.subplot(2, 1, 2)
+plt.subplot(2, 1, 1)
 plt.plot(x1, y2, label='f(x) = 2x + 3', color='orange')
 plt.title('График функции f(x) = 2x + 3')
 plt.grid(True)
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.legend()
-
 x2 = np.linspace(-2, 2, 100)
 y2 = np.linspace(-2, 2, 100)
 X, Y = np.meshgrid(x2, y2)
-Z = (X**2 + Y**2 - 1)**3 - X**2 * Y**3  # f(x, y)
-
-#Создание окна с одним графиком
-plt.figure(2)
-plt.contourf(X, Y, Z, levels=50, cmap='red')
+Z = (X ** 2 + Y ** 2 - 1) ** 3 - X ** 2 * Y ** 3
+# Создание окна с одним графиком
+plt.figure(2)  # Создаем второе окно
+plt.contour(X, Y, Z, levels=50, cmap='Reds')
 plt.colorbar()
 plt.title('График функции f(x, y) = (x^2 + y^2 - 1)^3 - x^2y^3')
 plt.xlabel('x')
 plt.ylabel('y')
 
+t = np.linspace(0, 2 * np.pi, 1000)
+heart_x = 16 * np.sin(t) ** 3
+heart_y = 13 * np.cos(t) - 5 * np.cos(2 * t) - 2 * np.cos(3 * t) - np.cos(4 * t)
+plt.plot(heart_x, heart_y, color='red', linewidth=3)
+plt.axis('equal')
 plt.show()
